cfe28538e95b171fc7d69e5c08ff552f26c4d696
Mohammad Mizanur Rahman <rshelley.76@gmail.com>
2016-02-27 10:59:36 -0500

    Seperate Saving Class to Seperate File

---
+++ InClassBankAccount2/InClassBankAccount2/Saving.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassBankAccount2
+{
+    public class Saving : Account
+    {
+        public Saving(string accNumber, string accName, decimal initialAmount)
+
+        {
+            base.accountNumber = accNumber;
+            base.accountName = accName;
+            base.Deposit(initialAmount);
+            base.accountType = Acc_Type.Saving;
+
+
+        }
+
+    }
+}
