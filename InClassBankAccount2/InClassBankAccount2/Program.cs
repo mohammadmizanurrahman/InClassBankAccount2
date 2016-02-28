cafa2c497c1a76078654f8ae3e6c28c9bc800f9f
Mohammad Mizanur Rahman <rshelley.76@gmail.com>
2016-02-28 02:34:06 -0500

    Sabrina Add print her name at console

--- InClassBankAccount2/InClassBankAccount2/Program.cs
+++ InClassBankAccount2/InClassBankAccount2/Program.cs
@@ -7,12 +7,13 @@ using System.Threading.Tasks;
 namespace InClassBankAccount2
 {
    //New Branch For Dependency Injection
-   // Changed by Lima
+   // Changed by Lima Ahmed
     class Program
     {
         static void Main(string[] args)
         {
             //
+            Console.WriteLine("Sabrina Ahmed");
 
             Saving Sv1 = new Saving("001", "Sette", 300.00m);
             Chequing Chq1 = new Chequing("001", "Mizan", 200.00m);
