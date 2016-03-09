e234228aefb32277fca4f152e2dd1a53ba46caa7
Mohammad Mizanur Rahman <rshelley.76@gmail.com>
2016-03-09 10:46:54 -0500

    Nick Name Added By Sabrina

--- InClassBankAccount2/InClassBankAccount2/Program.cs
+++ InClassBankAccount2/InClassBankAccount2/Program.cs
@@ -13,7 +13,7 @@ namespace InClassBankAccount2
         static void Main(string[] args)
         {
             //
-            Console.WriteLine("Sabrina Ahmed");
+            Console.WriteLine("Sabrina Ahmed Lima");
 
             Saving Sv1 = new Saving("001", "Sette", 300.00m);
             Chequing Chq1 = new Chequing("001", "Mizan", 200.00m);
